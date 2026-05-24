4b1817601fea0b3510d4700b71a450ed455252ac
Marko Trnjak <trrnjak@gmail.com>
2026-05-25 00:14:12 +0200

    Removed Venturemacs-theme.el

--- init.el
+++ init.el
@@ -25,6 +25,7 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (setq evil-want-keybinding nil)
+(setq evil-want-integration t)
 (unless (package-installed-p 'evil)
   (package-install 'evil))
 (require 'evil)
@@ -51,16 +52,12 @@
 (load-theme 'Venturemacs-light t)
 
 (require 'org)
-(org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+(let ((org-file (expand-file-name "README.org" user-emacs-directory))
+      (el-file (expand-file-name "README.el" user-emacs-directory)))
+  (if (file-newer-than-file-p org-file el-file)
+      (org-babel-load-file org-file)
+    (load el-file)))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
