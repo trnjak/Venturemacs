e5f55204c869ff25eda2be63c5bd2e54987bff8f
Marko Trnjak <trrnjak@gmail.com>
2026-05-24 16:36:48 +0200

    Added packages.

--- init.el
+++ init.el
@@ -24,6 +24,7 @@
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
+(setq evil-want-keybinding nil)
 (unless (package-installed-p 'evil)
   (package-install 'evil))
 (require 'evil)
@@ -57,3 +58,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
